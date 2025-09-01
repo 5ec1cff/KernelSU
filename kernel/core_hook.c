23b1c7d15d2176d1b8ebbdd4785a427eb08beca7
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-01 19:33:08 +0800

    x

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -283,7 +283,6 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 	pr_info("option: 0x%x, cmd: %ld\n", option, arg2);
 #endif
 
-	#define CMD_ADD_TRY_UMOUNT 10001 // change me later
 	if (arg2 == CMD_ADD_TRY_UMOUNT) {
 		struct mount_entry *new_entry, *entry;
 		char buf[384];
