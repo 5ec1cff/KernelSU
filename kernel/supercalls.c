6065b7ec23138636d81f4737fb753b258c579b4b
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2025-11-06 22:53:40 +0800

    refactor: rename proxy to wrapper

--- kernel/supercalls.c
+++ kernel/supercalls.c
@@ -19,7 +19,7 @@
 #include "selinux/selinux.h"
 #include "core_hook.h"
 #include "objsec.h"
-#include "proxy_file.h"
+#include "file_wrapper.h"
 
 // Permission check functions
 bool only_manager(void)
@@ -324,8 +324,6 @@ static int do_set_feature(void __user *arg)
     return 0;
 }
 
-extern struct file_operations mksu_proxy_file_ops;
-
 static int do_get_wrapper_fd(void __user *arg) {
 	if (!ksu_file_sid) {
 		return -1;
@@ -344,40 +342,40 @@ static int do_get_wrapper_fd(void __user *arg) {
 		return -EBADF;
 	}
     
-    struct ksu_file_proxy_data *data = ksu_make_proxy(f);
+    struct ksu_file_wrapper *data = mksu_create_file_wrapper(f);
     if (data == NULL) {
         ret = -ENOMEM;
         goto put_orig_file;
     }
 
-	struct file* pf = anon_inode_getfile("[mksu_proxy]", &data->ops, data, f->f_flags);
+	struct file* pf = anon_inode_getfile("[mksu_fdwrapper]", &data->ops, data, f->f_flags);
     if (IS_ERR(pf)) {
         ret = PTR_ERR(pf);
-        pr_err("get_wrapper_fd: anon_inode_getfile failed: %ld\n", PTR_ERR(pf));
-        goto put_proxy_data;
+        pr_err("mksu_fdwrapper: anon_inode_getfile failed: %ld\n", PTR_ERR(pf));
+        goto put_wrapper_data;
     }
 
-    struct inode* proxy_inode = file_inode(pf);
-    struct inode_security_struct *sec = selinux_inode(proxy_inode);
+    struct inode* wrapper_inode = file_inode(pf);
+    struct inode_security_struct *sec = selinux_inode(wrapper_inode);
     if (sec) {
         sec->sid = ksu_file_sid;
     }
 
     ret = get_unused_fd_flags(cmd.flags);
     if (ret < 0) {
-        pr_err("get_wrapper_fd: get unused fd failed: %d\n", ret);
-        goto put_proxy_file;
+        pr_err("mksu_fdwrapper: get unused fd failed: %d\n", ret);
+        goto put_wrapper_file;
     }
 
-    pr_info("get_wrapper_fd: installed proxy file for %p %d (flags=%d, mode=%d) to %p %d (flags=%d, mode=%d)", f, cmd.fd, f->f_flags, f->f_mode, pf, ret, pf->f_flags, pf->f_mode);
-    pf->f_mode |= FMODE_READ | FMODE_CAN_READ | FMODE_WRITE | FMODE_CAN_WRITE;
+    // pr_info("mksu_fdwrapper: installed wrapper fd for %p %d (flags=%d, mode=%d) to %p %d (flags=%d, mode=%d)", f, cmd.fd, f->f_flags, f->f_mode, pf, ret, pf->f_flags, pf->f_mode);
+    // pf->f_mode |= FMODE_READ | FMODE_CAN_READ | FMODE_WRITE | FMODE_CAN_WRITE;
 	fd_install(ret, pf);
     goto put_orig_file;
 	
-put_proxy_file:
+put_wrapper_file:
 	fput(pf);
-put_proxy_data:
-    ksu_delete_proxy(data);
+put_wrapper_data:
+    mksu_delete_file_wrapper(data);
 put_orig_file:
     fput(f);
 
