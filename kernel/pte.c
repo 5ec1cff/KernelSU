40dfd5993e660b38111957ffd2407713b0fdb36a
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-01-02 02:02:55 +0000

    2

--- kernel/pte.c
+++ kernel/pte.c
@@ -14,10 +14,14 @@ pte_t *page_from_virt(unsigned long addr)
     pgd = pgd_offset(mm, addr);
     if (pgd_none(*pgd) || pgd_bad(*pgd))
         return NULL;
+    pr_info("pgd of 0x%lx p=0x%lx v=0x%lx", addr, (uintptr_t)pgd,
+            (uintptr_t)pgd_val(*pgd));
 
     p4d = p4d_offset(pgd, addr);
     if (p4d_none(*p4d) || p4d_bad(*p4d))
         return NULL;
+    pr_info("p4d of 0x%lx p=0x%lx v=0x%lx", addr, (uintptr_t)p4d,
+            (uintptr_t)p4d_val(*p4d));
 
     pud = pud_offset(p4d, addr);
 #if defined(pud_leaf)
@@ -30,6 +34,8 @@ pte_t *page_from_virt(unsigned long addr)
 #endif
     if (pud_none(*pud) || pud_bad(*pud))
         return NULL;
+    pr_info("pud of 0x%lx p=0x%lx v=0x%lx", addr, (uintptr_t)pud,
+            (uintptr_t)pud_val(*pud));
 
     pmd = pmd_offset(pud, addr);
 #if defined(pmd_leaf)
@@ -40,6 +46,8 @@ pte_t *page_from_virt(unsigned long addr)
         return (pte_t *)pmd;
     }
 #endif
+    pr_info("pmd of 0x%lx p=0x%lx v=0x%lx", addr, (uintptr_t)pmd,
+            (uintptr_t)pmd_val(*pmd));
 
     if (pmd_none(*pmd) || pmd_bad(*pmd))
         return NULL;
